aaf71f94627db7eaf04c7e75ae8688804d243011
Charles Heller <charles.heller@hotmail.de>
2023-12-20 06:25:11 +0100

    testing new protecting rule

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className=" flex justify-center items-center min-h-screen min-w-screen bg-red-700">
       <h1>Streaming Website</h1>
+      <h2>This will be fun</h2>
     </main>
   );
 }
