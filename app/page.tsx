6ad05b237f42499f45a1c838fb18bfca445beacb
Charles Heller <charles.heller@hotmail.de>
2023-12-20 06:10:49 +0100

    added next js project, deleted example project from vercel

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className=" flex justify-center items-center min-h-screen min-w-screen bg-red-700">
+      <h1>Streaming Website</h1>
+    </main>
+  );
+}
